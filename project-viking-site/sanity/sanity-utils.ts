be565d2386158d0e9d3af470b0e0a4be4f88b197
Wendy Vasquez <wndvsq2@gmail.com>
2024-03-12 16:00:44 -0700

    add revalidation code to getPages call

--- project-viking-site/sanity/sanity-utils.ts
+++ project-viking-site/sanity/sanity-utils.ts
@@ -20,17 +20,19 @@ export async function getProjects(): Promise<Project[]>{
 }
 
 export async function getPages(): Promise<Page[]> {
+    const revalidate = 60
     return createClient(clientConfig).fetch(
       groq`*[_type == "page"]{
         _id,
         _createdAt,
         title,
         "slug": slug.current
-      }`
-    )
+      }`,{},
+     { next: {revalidate:revalidate}})
   }
   
   export async function getPage(slug: string): Promise<Page> {
+    
     return createClient(clientConfig).fetch(
       groq`*[_type == "page" && slug.current == $slug][0]{
         _id,
