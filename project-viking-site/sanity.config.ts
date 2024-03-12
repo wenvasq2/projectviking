511efb61159574eca0200ab9a76d7d45635737cc
Wendy Vasquez <wndvsq2@gmail.com>
2024-03-12 02:01:50 -0700

    studio integration to website project

---
+++ project-viking-site/sanity.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure"; //top level view of sanity
+//import schemas from "./sanity/schemas";
+
+const config = defineConfig({
+    projectId: "8tm7x9su",
+    dataset: "production",
+    title: "Demo Site title",
+    apiVersion: "2024-02-13",
+    basePath: "/admin",
+    plugins: [structureTool()],
+   // schema: {types: schemas}
+})
+
+export default config;
